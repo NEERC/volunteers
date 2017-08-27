08ccb5b48c7f719a9359d212e7fddc0c54ac6d97
aleksej-lapenok <lapenok.aleksej@gmail.com>
2017-08-27 16:42:36 +0300

    merge with master

--- src/main/java/ru/ifmo/neerc/volunteers/controller/YearController.java
+++ src/main/java/ru/ifmo/neerc/volunteers/controller/YearController.java
@@ -1,7 +1,5 @@
 package ru.ifmo.neerc.volunteers.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +10,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.thymeleaf.spring.support.Layout;
-
 import ru.ifmo.neerc.volunteers.entity.ApplicationForm;
-import ru.ifmo.neerc.volunteers.repository.PositionRepository;
+import ru.ifmo.neerc.volunteers.entity.Year;
+import ru.ifmo.neerc.volunteers.repository.PositionValueRepository;
 import ru.ifmo.neerc.volunteers.repository.YearRepository;
+
+import javax.validation.Valid;
 
 @Controller
 @Layout("public")
 public class YearController {
 
     @Autowired
-    PositionRepository positionRepository;
+    PositionValueRepository positionValueRepository;
 
     @Autowired
     YearRepository yearRepository;
@@ -35,8 +35,9 @@ public class YearController {
 
     @GetMapping("/years/{id}/signup")
     public String signupForYear(@PathVariable long id, Model model) {
-        model.addAttribute("year", yearRepository.findOne(id));
-        model.addAttribute("positions", positionRepository.findAll());
+        Year year = yearRepository.findOne(id);
+        model.addAttribute("year", year);
+        model.addAttribute("positions", year.getPositionValues());
 
         if (!model.containsAttribute("applicationForm")) {
             model.addAttribute("applicationForm", new ApplicationForm());
